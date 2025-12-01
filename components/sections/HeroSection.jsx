7ac311d1182cca807080704eae505513be385a13
rober <robertojesusvasquez@gmail.com>
2025-12-01 13:29:29 -0300

    viewport fix

--- components/sections/HeroSection.jsx
+++ components/sections/HeroSection.jsx
@@ -31,7 +31,7 @@ const HeroSection = ({ isLoading }) => {
     }, [isLoading])
 
     return (
-        <section id="home" className="min-h-[100dvh] flex flex-col justify-center items-center px-4 md:px-16 py-20 relative overflow-hidden">
+        <section id="home" className="min-h-[calc(var(--vh,1vh)*100)] flex flex-col justify-center items-center px-4 md:px-16 py-20 relative overflow-hidden">
 
             {/* Gradient fade effect at bottom */}
             <div className="absolute bottom-0 left-0 right-0 h-64 bg-gradient-to-t from-blue-100/60 via-purple-100/40 to-transparent pointer-events-none"></div>
