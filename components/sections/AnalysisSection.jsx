0397c13ac30062d16cb3bb6cd5b3da64be2c1285
rober <robertojesusvasquez@gmail.com>
2025-12-01 13:24:32 -0300

    viewport fix

--- components/sections/AnalysisSection.jsx
+++ components/sections/AnalysisSection.jsx
@@ -8,7 +8,7 @@ const AnalysisSection = () => {
     const { t } = useLanguage()
 
     return (
-        <section className="min-h-[100dvh] px-4 md:px-16 py-32 relative" style={{ backgroundColor: 'var(--background)' }}>
+        <section className="min-h-[calc(var(--vh,1vh)*100)] px-4 md:px-16 py-32 relative" style={{ backgroundColor: 'var(--background)' }}>
 
             {/* Step indicator 03 - Analysis */}
             <StepIndicator
